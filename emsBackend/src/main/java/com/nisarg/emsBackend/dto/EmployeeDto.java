698231ce1e020ab44a2f97271de9687a4dc242b5
Nisarg Makwana <nisarg.makwana204@gmail.com>
2026-07-18 19:58:46 +0530

    added dto and mapper class

---
+++ emsBackend/src/main/java/com/nisarg/emsBackend/dto/EmployeeDto.java
@@ -0,0 +1,20 @@
+package com.nisarg.emsBackend.dto;
+
+import jakarta.persistence.GeneratedValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeDto {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
---
+++ emsBackend/src/main/java/com/nisarg/emsBackend/mapper/EmployeeMapper.java
@@ -0,0 +1,26 @@
+package com.nisarg.emsBackend.mapper;
+
+import com.nisarg.emsBackend.dto.EmployeeDto;
+import com.nisarg.emsBackend.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+
+}
