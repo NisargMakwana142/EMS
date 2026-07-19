f3cf99745aba2f3b08f4f4cca3e7dda49505c9c3
Nisarg Makwana <nisarg.makwana204@gmail.com>
2026-07-19 14:39:53 +0530

    added controller class and created post method

---
+++ emsBackend/src/main/java/com/nisarg/emsBackend/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package com.nisarg.emsBackend.controller;
+
+
+import com.nisarg.emsBackend.dto.EmployeeDto;
+import com.nisarg.emsBackend.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+}
