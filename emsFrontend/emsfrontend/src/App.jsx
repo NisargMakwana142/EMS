fadd068e51323ea033efc0826a09a88cb48d0672
Nisarg Makwana <nisarg.makwana204@gmail.com>
2026-07-20 17:24:18 +0530

    added header & footer

--- emsFrontend/emsfrontend/src/App.jsx
+++ emsFrontend/emsfrontend/src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import FooterComponent from './components/FooterComponent'
+import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponents from './components/ListEmployeeComponents'
 import Helloworld from './Helloworld'
 
@@ -7,7 +9,9 @@ function App() {
 
   return (
     <>
+      <HeaderComponent />
       <ListEmployeeComponents />
+      <FooterComponent />
 
     </>
   )
